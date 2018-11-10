80711d37cb27f897932a11687184f3bf108ecb29
SPCBA-CorE <delossantosgiancarlo27@gmail.com>
2018-11-11 06:36:07 +0800

    Add files via upload

---
+++ src/main/java/edu/sfsu/cs/orange/ocr/model/ServiceAndBillingInfo.java
@@ -0,0 +1,17 @@
+package edu.sfsu.cs.orange.ocr.model;
+
+/**
+ * Created by serge.delossantos on 11/10/2018.
+ */
+
+public class ServiceAndBillingInfo {
+    public String ServiceIDNumber;
+    public String Rate;
+    public String ContractInTheNameOf;
+    public String ServiceAddress;
+    public String BillDate;
+    public String MeterReadingDate;
+    public String BillPeriod;
+    public String DueDate;
+    public String TotalKWH;
+}
