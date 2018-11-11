a7c4c4caa127ad08efc008cfa241e90b5568429f
SPCBA-CorE <delossantosgiancarlo27@gmail.com>
2018-11-11 09:52:18 +0800

    Update CaptureActivityHandler.java

--- src/main/java/edu/sfsu/cs/orange/ocr/CaptureActivityHandler.java
+++ src/main/java/edu/sfsu/cs/orange/ocr/CaptureActivityHandler.java
@@ -1,19 +1,4 @@
-/*
- * Copyright (C) 2008 ZXing authors
- * Copyright 2011 Robert Theis
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package edu.sfsu.cs.orange.ocr;
 
 import edu.sfsu.cs.orange.ocr.CaptureActivity;
@@ -27,11 +12,7 @@ import android.util.Log;
 import android.view.Gravity;
 import android.widget.Toast;
 
-/**
- * This class handles all the messaging which comprises the state machine for capture.
- *
- * The code for this class was adapted from the ZXing project: http://code.google.com/p/zxing/
- */
+
 final class CaptureActivityHandler extends Handler {
 
   private static final String TAG = CaptureActivityHandler.class.getSimpleName();
